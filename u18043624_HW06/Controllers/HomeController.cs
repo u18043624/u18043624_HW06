34d5e0de7b5122973488bb3b8f3983fc7683c3b8
u18043624 <102961181+u18043624@users.noreply.github.com>
2022-10-29 19:40:09 +0200

    Update 1
    Paging, partial views, model and working on modal

--- u18043624_HW06/Controllers/HomeController.cs
+++ u18043624_HW06/Controllers/HomeController.cs
@@ -3,28 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList.Mvc;
+using PagedList;
+using u18043624_HW06.Models
 
 namespace u18043624_HW06.Controllers
 {
 	public class HomeController : Controller
 	{
-		public ActionResult Index()
+		public ActionResult Orders()
 		{
 			return View();
 		}
 
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
-
-			return View();
-		}
-
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
-
-			return View();
-		}
 	}
 }
---
+++ u18043624_HW06/Models/ProductsCRUDClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u18043624_HW06.Models
+{
+	public class ProductsCRUDClass
+	{
+		public int product_id;
+		public string product_name;
+		public int brand_id;
+		public string category;
+		public int model_year;
+		public float list_price;
+
+	}
+}
