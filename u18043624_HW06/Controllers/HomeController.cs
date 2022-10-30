c3fa8f5b1b462ce5222b39da9a43f6343c322473
u18043624 <102961181+u18043624@users.noreply.github.com>
2022-10-30 23:55:38 +0200

    Update4

--- u18043624_HW06/Controllers/HomeController.cs
+++ u18043624_HW06/Controllers/HomeController.cs
@@ -16,11 +16,10 @@ namespace u18043624_HW06.Controllers
 
 		public ActionResult Orders(int? id, string search, int? i)
 		{
-			//var orders = from product_ in order_items orderby order_items.order_id
+			var orderIds = db.order_items.Select(x => x.order_id).ToList();
+			//var orders = 
 			//var orders = db.order_items.Where(p => p.order_id == order_items.order_id).ToList();
-			//List<production.orders>
-			//return View(db.order_items.Where(x => x.order.order_date.ToString().StartsWith(search) || search == null).ToList().ToPagedList(i ?? 1, 10)); //converted to string, is right?
-			return View(db.order_items.Where(x => x.product_name.StartsWith(search) || search == null).ToList().ToPagedList(i ?? 1, 10));
+			return View(db.order_items.Where(x => x.order.order_date.ToString().StartsWith(search) || search == null).ToList().ToPagedList(i ?? 1, 10)); //converted to string, is right?
 		}
 
 		public ActionResult Report()
